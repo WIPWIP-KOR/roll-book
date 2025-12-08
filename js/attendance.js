18f78e2da68ebe07748da4dbc9e3e25b4fa1d044
WIPWIP <65800532+WIPWIP-KOR@users.noreply.github.com>
2025-12-08 15:41:59 +0900

    Update attendance.js

--- js/attendance.js
+++ js/attendance.js
@@ -1,6 +1,6 @@
 // 설정 (배포 후 Google Apps Script Web App URL로 변경)
 const CONFIG = {
-    GAS_URL: 'https://script.google.com/macros/s/AKfycbz0qh_mWTEP3uvTeMzq4ZK7L56tu1eFcJWKP7mNZsOhRwAcu_qoDQqzWgHBD3YhZymr/exec', // 나중에 변경 필요
+    GAS_URL: 'https://script.google.com/macros/s/AKfycbxdCQsDUJgH0cmzso9v8SygAoT9UCOMCG0xpbs83dCqhhgVa8NJdf4L4SUSkcjG4F8x/exec', // 나중에 변경 필요
     REQUIRED_RADIUS: 50 // 50미터
 };
 
