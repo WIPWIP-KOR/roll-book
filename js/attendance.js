d2ee2ddec9a1025e90b9c39108aef61b6e347de6
WIPWIP <65800532+WIPWIP-KOR@users.noreply.github.com>
2025-12-08 23:48:25 +0900

    배포

--- js/attendance.js
+++ js/attendance.js
@@ -1,7 +1,7 @@
 // 설정
 const CONFIG = {
     // ⚠️⚠️⚠️ 여기를 실제 Google Apps Script 배포 URL로 변경하세요 ⚠️⚠️⚠️
-    GAS_URL: 'https://script.google.com/macros/s/AKfycbyb96h-C6dDUFbeSrIlO43YzkfYMz0cpa9N30fqjE8pESWeIFKMTa4P2W1V0tYhi6xw/exec', // 나중에 변경 필요
+    GAS_URL: 'https://script.google.com/macros/s/AKfycbxjmvZWEErrnhyGtgyhrpBAoy8lF_Cw7V9bJNgTBCRQKeFrkROu-tp43uAcSEu9VxBd/exec', // 나중에 변경 필요
     REQUIRED_RADIUS: 50 // 50m 이내만 출석 인정
 };
 
